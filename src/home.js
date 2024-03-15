616144c001e6a2f9cd6ac19bc32c8f643c74eb7a
tharun_raj <64694833+tharun-raj@users.noreply.github.com>
2024-03-15 13:04:59 +0530

    Update home.js

--- src/home.js
+++ src/home.js
@@ -1,26 +1,29 @@
-import BlogList from "./BlogList"
-import useFetch from "./useFetch"
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
+    const { data: blog, isPending, error } = useFetch("http://localhost:8000/blogs");
 
     const handleClick = () => {
-        navigate("/create")
+        navigate("/create");
     }
 
-    const {data:blog, isPending, error} = useFetch("http://localhost:8000/blogs")
-
-    return(
+    return (
         <div>
             {error && <h1 className="page-head"> {error} </h1>}
             {isPending && <h1 className="page-head"> Loading... </h1>}
-            {blog && <h1 className="page-head">HomePage</h1>}
-            {blog && <button onClick={handleClick}>Create Blog</button>}
-            {blog && <BlogList blogs = {blog} />}
+            {blog && (
+                <>
+                    <h1 className="page-head">HomePage</h1>
+                    <button onClick={handleClick}>Create Blog</button>
+                    <BlogList blogs={blog} />
+                </>
+            )}
         </div>
-    )
+    );
 }
 
 export default Home;
